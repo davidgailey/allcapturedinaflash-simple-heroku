a49055717f824a9421aa89a2cbd0c76ad0ba0bb7
David Gailey <dave.g.github@gmail.com>
2017-08-14 22:35:32 -0400

    reorganize files, header and footer, create separate 360 page and clear out index page

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/360', function(request, response) {
+  response.render('pages/360');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
